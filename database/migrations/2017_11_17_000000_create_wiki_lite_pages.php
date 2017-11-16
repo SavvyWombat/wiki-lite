807d443e4dfb8f71e6a41c5ca985d3dcf7f10880
Stuart Jones <stuart@savvywombat.com.au>
2017-11-17 10:18:41 +1030

    use integers instead of bigint (4 billion is a big number) - allow for users

--- database/migrations/2017_11_17_000000_create_wiki_lite_pages.php
+++ database/migrations/2017_11_17_000000_create_wiki_lite_pages.php
@@ -14,9 +14,12 @@ class CreateWikiLitePages extends Migration
     public function up()
     {
         Schema::create('wiki_lite_pages', function(Blueprint $table) {
-            $table->bigIncrements('revision');
+            $table->increments('revision');
             $table->uuid('id');
-            $table->uuid('parent_id');
+            $table->uuid('parent_id')
+                ->nullable();
+            $table->integer('user_id')
+                ->default(0);
             $table->dateTimeTz('updated_at');
             $table->text('title');
             $table->text('slug');
