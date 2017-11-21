cc947e8fff53fe9d28ddd3f307777c972122ef4d
Stuart Jones <stuart@savvywombat.com.au>
2017-11-22 08:02:56 +1030

    added Orchestra\TestBench and create Feature\TestCase

---
+++ tests/Feature/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SavvyWombat\WikiLite\Tests\Feature;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            'SavvyWombat\WikiLite\ServiceProvider',
+        ];
+    }
+
+    /**
+     * Get package aliases.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            // No facades yet
+        ];
+    }
+}
