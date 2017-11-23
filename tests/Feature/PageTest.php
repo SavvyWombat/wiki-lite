737357cc2145ea84bcd0756137e7561cef525a60
Stuart Jones <stuart@savvywombat.com.au>
2017-11-23 19:50:22 +1030

    extend test coverage to include SavePage Request rules and messages

--- tests/Feature/PageTest.php
+++ tests/Feature/PageTest.php
@@ -21,6 +21,8 @@ class PageTest extends TestCase
 
     /**
      * @covers SavvyWombat\WikiLite\Controllers\PageController::save
+     * @covers SavvyWombat\WikiLite\Requests\SavePage::rules
+     * @covers SavvyWombat\WikiLite\Requests\SavePage::messages
      * @uses SavvyWombat\WikiLite\Requests\SavePage
      */
     public function testSave_ensureRequiredContent()
